6d4b6380223e7250b5f79df6985db99921dfcd63
PremiumSubscriptions <help.infodesk.premium@gmail.com>
2025-12-26 00:36:32 +0600

    Create initial App component for Live Exam Portal

---
+++ App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div style={{padding:20}}>
+      <h2>Live Exam Portal</h2>
+
+      <p>1. What is g?</p>
+      <label><input type="radio" name="q1"/> 9.8</label><br/>
+      <label><input type="radio" name="q1"/> 10</label><br/>
+
+      <button>Submit Exam</button>
+    </div>
+  );
+}
+
+export default App;
